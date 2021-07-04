e15b2fbfdcdd17c4462be79592954177b494e6a2
mohanapravallika <84385928+mohanapravallika@users.noreply.github.com>
2021-07-04 21:34:49 +0530

    Add files via upload

---
+++ Home_Address.java
@@ -0,0 +1,17 @@
+package pack1;
+public class Home_Addr
+{
+    public static void main(String[] args) 
+    {
+     String name = "usha";
+     String dno = "2-3-22";
+     String city = "rajahmundry";
+     String state = "ap";
+     System.out.println("Address :");
+     System.out.println("---------");
+     System.out.println("Name = "+name);
+     System.out.println("DNO = "+dno);
+     System.out.println("City = "+city);
+     System.out.println("State = "+state);
+     }
+}
