2ccf759a2e1c9d355d781b8d8cc28645670f5cbd
mohanapravallika <84385928+mohanapravallika@users.noreply.github.com>
2021-07-04 21:35:31 +0530

    Add files via upload

---
+++ Opt6_L_R.java
@@ -0,0 +1,10 @@
+package pack1;
+public class Op6_L_R_shft {
+	public static void main(String[] args) {
+		System.out.println(100<<2);
+		System.out.println(30<<3);
+		System.out.println(100>>2);
+		System.out.println(20>>3);
+        System.out.println(20>>>3);
+	}
+}
